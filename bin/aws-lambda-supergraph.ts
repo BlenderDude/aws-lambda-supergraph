014343edd01f8e15ca945c4ee875b78623ceff1c
Daniel Abdelsamed <daniel@apollographql.com>
2023-08-04 13:31:36 -0400

    Pipeline

--- bin/aws-lambda-supergraph.ts
+++ bin/aws-lambda-supergraph.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AWSLambdaSupergraphStack } from '../lib/aws-lambda-supergraph-stack';
+import { CDKPipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new AWSLambdaSupergraphStack(app, 'LambdaSupergraph', {});
+new CDKPipelineStack(app, 'CDKPipelineStack', {})
