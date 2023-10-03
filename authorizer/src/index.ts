580c09527a1dcb5da9a757d349a3d569b1809037
Daniel Abdelsamed <daniel@abdelsamed.com>
2023-10-02 23:26:08 -0400

    Lowercase T lol

--- authorizer/src/index.ts
+++ authorizer/src/index.ts
@@ -43,7 +43,7 @@ export const handler: APIGatewayRequestSimpleAuthorizerHandlerV2WithContext<{
 }> = async (e) => {
   const secret = await getSecret();
 
-  if (!validateRouterToken(e.headers?.["x-router-Token"] ?? "", secret)) {
+  if (!validateRouterToken(e.headers?.["x-router-token"] ?? "", secret)) {
     console.log("Failing on bad router token");
     return {
       isAuthorized: false,
